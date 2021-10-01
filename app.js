4048f51cc333bb585bcb825da599c20e4d0c72ab
TheCoolDev <thecoolgdev@gmail.com>
2021-10-01 19:27:42 +0545

    Added files

---
+++ app.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const argv = require("yargs").argv;
+require("dotenv").config();
+
+let apiKey = process.env.apiKey;
+let city = argv.c || "London";
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log("error:", error);
+  } else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
